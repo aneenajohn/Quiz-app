1f7500a35e093f7f56f12356ae89b092f7181b03
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-06-04 11:59:43 +0530

    Added data loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./styles.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Quiz } from "./Pages/Quiz";
+import { DataLoader } from "./dataLoader";
 
 export default function App() {
   return (
     <div className="App">
       <h1>World Of Words</h1>
+      <DataLoader />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
